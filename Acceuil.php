397c4cf9137f1685f88849c75b1370f9d5272336
GuillaumeCrmt <courmontguillaume@gmail.com>
2015-03-20 18:55:50 +0100

    Debut, menu, background titre

---
+++ Acceuil.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+
+<html lang="fr" xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" type="text/css" href="image.css">
+    <title>
+    	Site Perso
+    </title>
+</head>
+
+<body>
+	<img src="http://www.vegmarine.fr/wp-content/uploads/2013/02/1001pharmacies-medicaments.jpg" width="150" height="150">
+
+	<div id="titre">
+		<h1>Pharmacie Yves Laurent</h1>
+	</div>
+	</br>
+
+	<form>
+		<table border="1">
+			<td>
+				<h3>Menu</h3></br>
+				<a href="Acceuil.php">Acceuil</a></br>
+				<a href="Orthopedie.php">Orthopédie</a></br>
+				<a href="Materiel.php">Materiel Medical</a></br>
+				<a href="Aroma.php">Aromatherapie</a></br>
+				<a href="Para.php">Parapharmacie</a></br>
+				<a href="Contacter.php">Nous Contacter</a></br>
+			</td>
+		</table>
+	</form>
+</body>
+
+</html>
